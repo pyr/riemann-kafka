62002b0766e4f2d3bf1749261c36bd2f8bd7e967
Pierre-Yves Ritschard <pyr@spootnik.org>
2015-02-11 16:46:46 +0100

    correct paths

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.spootnik/riemann-kafka "0.1.0"
+(defproject org.spootnik/riemann-kafka "0.1.1"
   :description "riemann producer and consumer for kafka queues"
   :url "https://github.com/pyr/riemann-kafka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [riemann             "0.2.4"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [riemann             "0.2.7"]
                  [clj-kafka           "0.1.2-0.8"
                   :exclusions [org.slf4j/slf4j-log4j12
                                org.slf4j/slf4j-simple]]])
